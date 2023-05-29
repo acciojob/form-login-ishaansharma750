c8d4dcf73d71a03bb2f3995b2854a6de81f9defb
Suansh Budhiraja <43946155+ishaansharma750@users.noreply.github.com>
2023-05-30 01:28:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
+	let last  = document.getElementById("lname");
+	let first = document.getElementById("fname");
+	// console.
+	alert(first.value+" "+ last.value);
+
 
 }
+
+
